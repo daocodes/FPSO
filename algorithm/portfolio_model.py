a302e006aa6e8850c7881b587bb733cec0b565f7
Dao Bui <tdaobui2025@gmail.com>
2026-07-11 23:43:18 -0400

    commit

---
+++ algorithm/portfolio_model.py
@@ -0,0 +1,14 @@
+#This file will implement the mathematical environment, objectives, and penalization structures of the paper
+import numpy as np
+
+#This is a function of the weights
+#lambda values will get tuned by the backtester
+def calculate_portfolio_return(weights: np.ndarray, expected_returns: np.ndarray, covariance: np.ndarray, weights_prev: np.ndarray, lambda_v: float, lambda_t: float):
+   
+
+
+    return portfolio_return
+    pass
+
+
+
